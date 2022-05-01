042f56b187fc932642019e46be4ba27616e231e4
yhc <2580577036@qq.com>
2022-05-01 11:07:55 +0800

    UI of remmend-part -f

--- src/components/common/navbaritem/NavBarItemLeft.vue
+++ src/components/common/navbaritem/NavBarItemLeft.vue
@@ -1,29 +1,71 @@
 <template>
-    <div class="navbar-item">
-        <div>推荐音乐页面</div>
+  <div class="navbar-item">
+    <div class="recomend-list">
+      <h2>推荐歌单</h2>
     </div>
+    <recommend-item
+      v-for="(item, index) in recommendSongList"
+      :dataItem="item"
+      :key="index"
+    ></recommend-item>
+  </div>
 </template>
 
 <script>
+import { getRecommendSongList, getNewSong } from "@/network/home";
+import RecommendItem from "./RecommendItem.vue";
+
 export default {
-    name: 'FunsMusicMobileNavbaritem',
+  components: { RecommendItem },
+  name: "NavbarItem",
 
-    data() {
-        return {
-            
-        };
-    },
+  data() {
+    return {
+      recommendSongList: [],
+      newSong: {},
+    };
+  },
 
-    mounted() {
-        
-    },
+  created() {
+    getRecommendSongList().then((res) => {
+      let len = 9 < res.result.length ? 9 : res.result.length;
+      for (let i = 0; i < len; i++) {
+        // 取推荐的前九个歌单,不多于9个
+        this.recommendSongList.push(res.result[i]);
+      }
+      console.log(this.recommendSongList);
+    });
+  },
 
-    methods: {
-        
-    },
+  mounted() {},
+
+  methods: {},
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style  scoped>
+.recomend-list {
+  position: relative;
+}
+.recomend-list h2 {
+  position: relative;
+  font-size: 18px;
+  text-align: left;
+  padding-left: 9px;
+  height: 40px;
+  line-height: 40px;
+}
+.recomend-list h2:before {
+  position: absolute;
+  content: "";
+  background-color: #ec8537;
+  width: 4px;
+  height: 18px;
+  left: 0;
+  top: 50%;
+  margin-top: -8px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+}
 </style>
---
+++ src/components/common/navbaritem/RecommendItem.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="remmend-item">
+        <div class="image">
+            <img :src="dataItem.picUrl" alt="" class="ge_dan">
+            <img src="../../../assets/img/music.png" alt="" class="icon-l">
+            <span> {{ playCount }} </span>
+        </div>
+        <div class="text">{{dataItem.name}}</div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RecommendItem',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    props: {
+        dataItem: {},
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+    computed: {
+        // 歌单播放量
+        playCount() {
+            let res = this.dataItem.playCount;
+            if(this.dataItem.playCount > 10000) {
+                res = parseInt(this.dataItem.playCount / 10000) + "万"
+            }
+            return res;
+        }
+    }
+};
+</script>
+
+<style  scoped>
+.remmend-item {
+    padding-right: 5px;
+    position: relative;
+    width: 33%;
+    float: left;
+
+}
+.ge_dan {
+    width: 100%;
+    border-radius: 5px;
+}
+.image span {
+    position: absolute;
+    font-size: 12px;
+    top: 4px;
+    right: 8%;
+    z-index: 1;
+    color: var(--color-high-text);
+}
+.icon-l {
+    width: 20px;
+    position: absolute;
+    top: 1px;
+    right: 33%;
+}
+.text {
+    font-size: 13px;
+    overflow: hidden;
+    height: 30px;
+}
+
+</style>
